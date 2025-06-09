3f5a1196fa31996fbf939751504507c8640c7f9a
João Altafini <joao.batista16@fatec.sp.gov.br>
2025-06-09 20:57:22 -0300

    Teste

--- view/cadastro_professor.php
+++ view/cadastro_professor.php
@@ -41,8 +41,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link rel="icon" type="image/x-icon" href="../assets/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro - SISESCOLAR</title>
-    <link href="../Assets/css/style.css" rel="stylesheet">
+    <link href="estilo.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    
 </head>
 <body>
     <div class="container mt-5">
